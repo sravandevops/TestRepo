885934fc12deab5cd686b6b35bc01e6ca1bb465b
root <root@ip-10-1-1-100.ec2.internal>
2019-06-07 12:24:36 +0000

    Added echo for user creation

--- user.sh
+++ user.sh
@@ -4,3 +4,4 @@ read -p "Please Enter The User Name:" USERNAME
 echo $USERNAME
 read -s -p "Please Enter The Password:" PASSWORD
 useradd -m $USERNAME -p $PASSWORD
+echo "User $USERNAME is created successfully....!!!"
